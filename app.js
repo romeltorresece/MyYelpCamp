9c11311855b509931289999a011b5f9ff0a66f37
Romel Torres <romeltorres.ece@gmail.com>
2022-04-29 20:28:40 +0800

    YelpCamp: Adding in Authentication

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
+const User = require('./models/user');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
     .then(() => {
-        console.log('DATABASE CONNECTED!');
+        console.log('MONGO DATABASE CONNECTED!');
     })
     .catch(err => {
-        console.log('DB CONNECTION ERROR!');
+        console.log('MONGO DB CONNECTION ERROR!');
         console.log(err);
     });
 
-const app = express();
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
 
 app.engine('ejs', ejsMate);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // express-session
 const sessionConfig = {
@@ -41,18 +48,32 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    // if (req.originalUrl !== '/login' && req.originalUrl !== '/register' && req.originalUrl !== '/') {
+    //     req.session.returnTo = req.originalUrl;
+    // }
+    if (!['/login', '/register', '/'].includes(req.originalUrl)) {
+        req.session.returnTo = req.originalUrl;
+    }
+    // console.log(req.session.returnTo);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 });
 
 // Routes
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 
 app.get('/', (req, res) => {
     res.render('home');
