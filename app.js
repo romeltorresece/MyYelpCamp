22c2e8bbd1d84f827d4257ee6071e396467f79d0
Romel Torres <romeltorres.ece@gmail.com>
2022-05-20 09:33:19 +0800

    YelpCamp: Common Security Issues

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const LocalStrategy = require('passport-local');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 mongoose.connect('mongodb://localhost:27017/my-yelp-camp')
     .then(() => {
@@ -28,21 +30,23 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride('_method'));
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.use(mongoSanitize({ replaceWith: '_' }));
 
 // express-session
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        // secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -50,6 +54,52 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
+app.use(helmet({ crossOriginEmbedderPolicy: false }));
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+            "'self'",
+            "blob:",
+            "data:",
+            "https://res.cloudinary.com/dbhv1lwyz/", // SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+            "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+        childSrc: ["blob:"],
+    }
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
--- schemas.js
+++ schemas.js
@@ -1,19 +1,45 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHtml': '{{#label}} must not include HTML!',
+    },
+    rules: {
+        escapeHtml: {
+            validate(value, helpers) {
+                const opts = (tags, attr) => ({
+                    allowedTags: tags,
+                    allowedAttributes: attr
+                });
+                const filtered = sanitizeHtml(value, opts(false, false)); // allow all tags and attributes
+                const clean = sanitizeHtml(value, opts([], {})); // don't allow all tags and all attributes
+                // compare the sanitized values, if there are HTML tags in the orig value, run helpers.error
+                // will allow other special characters (&, <, >, ...)
+                if (clean !== filtered) return helpers.error('string.escapeHtml');
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
-        title: Joi.string().required(),
-        location: Joi.string().required(),
+        title: Joi.string().required().escapeHtml(),
+        location: Joi.string().required().escapeHtml(),
         price: Joi.number().min(0).required(),
         // image: Joi.string().required(),
-        description: Joi.string().required()
+        description: Joi.string().required().escapeHtml()
     }).required(),
     deleteImages: Joi.array()
 });
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required().min(1).max(5),
-        body: Joi.string().required()
+        rating: Joi.number().min(1).max(5).required(),
+        body: Joi.string().required().escapeHtml()
     }).required()
 });
